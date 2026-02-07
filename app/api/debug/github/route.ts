04b696a93a719edb99eb505d8c186223c375c099
TridibeshSam31 <tridisam651@gmail.com>
2026-02-07 14:48:52 +0530

    done

--- app/api/debug/github/route.ts
+++ app/api/debug/github/route.ts
@@ -28,3 +28,18 @@ export async function GET() {
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
+
+
+
+/**
+ * 
+ * 
+ * 
+ * I created this folder because the earlier integration process was somehow wrong because I was using acess tokens instead of github token not talking about oAuth secreat tokens 
+ * the access tokens are of no use for integrating we have to generate the token masnually by going to developer settings on github so that we could get all the user information like how many repos are there , repos name 
+ * etc on a single api so we need to create that 
+ * 
+ *so what changes are made ???
+ replaced the github server actions , accesss.token with process.env.github / seperate github tokens 
+ added better error handlings by adding logs 
+ */
